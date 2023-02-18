9873cbf586e40b6e040b09b3999dedc5936cf387
Lucas <gibsonlc@mail.uc.edu>
2023-02-18 10:53:13 -0500

    Project init

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,12 +1,18 @@
 public class ShipCostCalculator {
     public static void main(String[] args) {
+        //Declare Variables
         double itemCost = 110;
         double shippingCost = itemCost * .02;
+        //Prompt
         System.out.println("What is your item cost?");
+        //Input
         System.out.println(+itemCost);
+        //Output For Free Shipping
         if (itemCost >= 100) {
             System.out.println("Your shipping is free for an item cost of $" +itemCost);
-        } else {
+        }
+        //Output For Shipping Cost
+        else {
             System.out.println("You shipping cost is $" +shippingCost+" for an item of $" +itemCost);
         }
     }
